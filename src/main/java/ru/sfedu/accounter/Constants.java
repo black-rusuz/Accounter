ad9287642b7a3779c2c3e90a7e8e2c63a25522bc
RUzai <RUzairov@gmail.com>
2021-12-04 23:14:18 +0300

    Added enviroment class and properties; Constants class for accessing them.

---
+++ src/main/java/ru/sfedu/accounter/Constants.java
@@ -0,0 +1,8 @@
+package ru.sfedu.accounter;
+
+public class Constants {
+    public static String APP_NAME = "APP_NAME";
+    public static String PATH_TO_HOME = "PATH_TO_HOME";
+
+    public static String XML_FILE = "XML_FILE";
+}
